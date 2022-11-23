d5de261ff647388a5dc1e93a4299d4c694759d80
Cherif436 <66637230+Cherif436@users.noreply.github.com>
2022-11-23 03:00:12 +0100

    Update Layouts.tsx

--- pages/Layouts.tsx
+++ pages/Layouts.tsx
@@ -25,6 +25,12 @@ const Layouts = () => {
       <nav className="flex items-center justify-center overflow-x-auto text-md sm:text-sm xs:text-xs">
         <Link href="/" className="p-1">   
         </Link>
+        <Link href="/" className="p-1">   
+        </Link>
+        <Link href="/" className="p-1">   
+        </Link>
+        <Link href="/" className="p-1">   
+        </Link>
         <Link href="/parcours" className="p-1 hover:text-red-500">
           Parcours
         </Link>
