b3e85e72ce7efc005c38b93d95569e308f11dc9e
Cherif436 <66637230+Cherif436@users.noreply.github.com>
2022-11-23 02:52:24 +0100

    Update Layouts.tsx

--- pages/Layouts.tsx
+++ pages/Layouts.tsx
@@ -22,7 +22,7 @@ const Layouts = () => {
     animate={{ opacity: 1 }}
     exit={{ opacity: 0 }}
     transition={{ duration: 1 }} className="overflow-y-scroll">
-      <nav className="flex items-center justify-center overflow-x-auto text-md sm:text-sm xs:text-xs">
+      <nav className="flex items-center justify-center overflow-x-auto text-md sm:text-sm xs:text-xs p-24">
         <Link href="/parcours" className="p-1 hover:text-red-500">
           Parcours
         </Link>
